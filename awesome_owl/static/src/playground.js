0bc9c1e3867aec1a6c724cf576e7eef3279d2ec4
WaqassAlii <97884227+waqassalii@users.noreply.github.com>
2025-12-30 02:55:41 +0500

    Step:5 added first component with counter method

---
+++ awesome_owl/static/src/playground.js
@@ -0,0 +1,19 @@
+/** @odoo-module **/
+
+import { Component, useState, markup } from "@odoo/owl";
+
+export class Playground extends Component {
+    static template = "awesome_owl.playground";
+
+    setup() { this.state = useState({ value: 0, }); }
+    increment() {
+        this.state.value++;
+    }
+    decrement = () => {
+        if ( this.state.value > 0) {
+        this.state.value--;
+    }
+    };
+
+
+}
