b79975c014b67d3a4808a7476e4d92a572e2c875
WaqasAli755 <waqassaliirandhawa@gmail.com>
2026-06-15 08:12:11 +0300

    Added the counter component

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -4,4 +4,10 @@ import { Component, useState, markup } from "@odoo/owl";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    setup(){
+        this.state = useState({value:0});
+    }
+    increment(){
+        this.state.value++;
+    }
 }
