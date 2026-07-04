db886adc1982c80245fd28538e854a7fd8a5f5ba
WaqassAlii <97884227+waqassalii@users.noreply.github.com>
2026-07-04 23:52:07 +0500

    added the dynamic props from play ground to cardDynamic

---
+++ awesome_owl/static/src/card/card_dynamic.js
@@ -0,0 +1,16 @@
+/** @odoo-module **/
+
+import { Component } from "@odoo/owl";
+
+export class CardDynamic extends Component {
+    static template = "awesome_owl.CardDynamic";
+    static props = {
+            title: String,
+            description: String,
+            price: { type: Number, optional: true },
+            imageUrl: String,
+            isPopular: Boolean,
+            year: Number,
+        };
+}
+
