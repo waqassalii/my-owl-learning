b0abd6df98464a3765e6a7a99521f34ffff01283
WaqassAlii <97884227+waqassalii@users.noreply.github.com>
2026-07-27 00:14:00 +0500

    [ADD] slot in card , minimizing the card content

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -4,6 +4,19 @@ import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.Card";
-    static props = ["title", "description"];
+    static props = {
+        title: { type: String, required: true },
+        description: { type: String, optional: true },
+        slots: {
+            type: Object,
+            optional: true,
+        },
+    };
+    setup() {
+        this.state = useState({ isOpen: true });
+    }
+    toggleContent() {
+        this.state.isOpen = !this.state.isOpen;
+    }
 }
 
