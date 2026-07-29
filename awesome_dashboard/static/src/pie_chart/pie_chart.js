0d2fa7145d71585293605d5af9497e758bb8c54b
WaqassAlii <97884227+waqassalii@users.noreply.github.com>
2026-07-29 23:51:42 +0500

    [add] pieChart using jsLoads inside onwillStart

---
+++ awesome_dashboard/static/src/pie_chart/pie_chart.js
@@ -0,0 +1,44 @@
+/** @odoo-module **/
+
+import { Component, onWillStart, onMounted, onWillUnmount, useRef } from "@odoo/owl";
+import { loadJS } from "@web/core/assets";
+
+export class PieChart extends Component {
+    static template = "awesome_dashboard.PieChart";
+    static props = {
+        label: String,
+        data: Object,
+    };
+
+    setup() {
+        // useRef we used it to get HTML element
+        this.canvasRef = useRef("canvas");
+        let chart;
+
+        // Lazy load Chart.js before the component renders
+        onWillStart(async () => {
+            await loadJS("/web/static/lib/Chart/Chart.js");
+        });
+
+        // Draw the chart once the canvas is actually on the screen
+        onMounted(() => {
+            chart = new Chart(this.canvasRef.el, {
+                type: "pie",
+                data: {
+                    labels: Object.keys(this.props.data),
+                    datasets: [{
+                        label: this.props.label,
+                        data: Object.values(this.props.data),
+                    }]
+                }
+            });
+        });
+
+        // Clean up memory when the component is removed when it is removed from screen
+        onWillUnmount(() => {
+            if (chart) {
+                chart.destroy();
+            }
+        });
+    }
+}
