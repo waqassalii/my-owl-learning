f41531c3f2a9a926daa9216f7d2b31dfe6b986b6
WaqassAlii <97884227+waqassalii@users.noreply.github.com>
2026-07-30 12:37:19 +0500

    [add] reactive to update data every 10 second

--- awesome_dashboard/static/src/statistics_service.js
+++ awesome_dashboard/static/src/statistics_service.js
@@ -2,18 +2,33 @@
 
 import { registry } from "@web/core/registry";
 import { memoize } from "@web/core/utils/functions";
+import { reactive } from "@odoo/owl";
 
 export const statisticsService = {
     dependencies: ["rpc"],
     start(env, { rpc }) {
+        // Create a standalone reactive object
+        const stats = reactive({});
         // memoize ensures /awesome_dashboard/statistics is only called ONCE
-        const loadStatistics = memoize(() => {
-            return rpc("/awesome_dashboard/statistics");
-        });
+        // const loadStatistics = memoize(() => {
+        //     return rpc("/awesome_dashboard/statistics");
+        // });
+        async function loadData() {
+            const data = await rpc("/awesome_dashboard/statistics");
+            Object.assign(stats, data); // Updates in place
+        }
+        // Initial load
+        loadData();
 
-        return {
-            loadStatistics,
-        };
+        // Reload every 10 seconds (10000 ms)
+        setInterval(loadData, 10000);
+
+        // Return the reactive object directly
+        return stats;
+
+        // return {
+        //     loadStatistics,
+        // };
     },
 };
 // Register our new service under the name "awesome_dashboard.statistics"
